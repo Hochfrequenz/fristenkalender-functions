2f806294b18c4ca920857f66d5d6c0ff806203ed
larisa <larisa.kosareva@hochfrequenz.de>
2023-06-29 15:09:06 +0200

    wip tests

--- unittests/test_generate_fristen_for_type.py
+++ unittests/test_generate_fristen_for_type.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 from typing import Optional
-import datetime
+
 import json
 
 import azure.functions as func
@@ -12,25 +12,19 @@ from GenerateFristenForType import main
 
 class TestGenerateFristenForType:
     @pytest.mark.parametrize(
-        "ok_request, expected_log_message",
+        "ok_request",
         [
             pytest.param(
                 func.HttpRequest(
                     "GET",
                     "testhost/GenerateFristenForType",
-                    route_params={"fristen_type": "GPKE"},
+                    route_params={"year": "2023", "fristen_type": "GPKE"},
                     body=bytes(),
-                ),
-                "Generating a list of fristen for the type GPKE ",
+                )
             ),
         ],
     )
-    def test_ok_get(
-        self,
-        caplog: LogCaptureFixture,
-        ok_request: func.HttpRequest,
-        expected_log_message: Optional[str],
-    ):
+    def test_ok_get(self, ok_request: func.HttpRequest):
         actual_response = main(ok_request)
         assert actual_response.status_code == HTTPStatus.OK
         actual_fristen_list = json.loads(actual_response.get_body().decode("utf-8"))
@@ -39,27 +33,23 @@ class TestGenerateFristenForType:
         assert actual_frist == expected
 
     @pytest.mark.parametrize(
-        "bad_request, expected_log_message",
+        "bad_request",
         [
             pytest.param(
                 func.HttpRequest(
                     "GET",
                     "testhost/GenerateFristenForType",
-                    route_params={"fristen_type": "hhhhhh"},
+                    route_params={"year": "2023", "fristen_type": "hhhhhh"},
                     body=bytes(),
-                ),
-                "Request parameter is invalid: 'hhhhhh' is not a valid fristen_type",
-                id="Wrong fristen type",
+                )
             ),
             pytest.param(
                 func.HttpRequest(
                     "GET",
                     "testhost/GenerateFristenForType",
-                    route_params={"fristen_type": ""},
+                    route_params={"year": "2023", "fristen_type": ""},
                     body=bytes(),
-                ),
-                "Request parameter must not be empty",
-                id="Empty parameter",
+                )
             ),
             pytest.param(
                 func.HttpRequest(
@@ -67,9 +57,8 @@ class TestGenerateFristenForType:
                     "testhost/GenerateFristenForType",
                     route_params=None,
                     body=bytes(),
-                ),
-                "Request parameter must not be none",
-                id="none parameter",
+                )
+                
             ),
             pytest.param(
                 func.HttpRequest(
@@ -77,26 +66,19 @@ class TestGenerateFristenForType:
                     "testhost/GenerateFristenForType",
                     route_params={},
                     body=bytes(),
-                ),
-                "Empty route params",
-                id="Empty route params",
+                )
             ),
             pytest.param(
                 func.HttpRequest(
                     "GET",
                     "testhost/GenerateFristenForType",
                     body=bytes(),
-                ),
-                "No route params",
-                id="No route params",
+                )
+                
             ),
         ],
     )
-    def test_bad_request(
-        self,
-        caplog: LogCaptureFixture,
-        bad_request: func.HttpRequest,
-        expected_log_message: Optional[str],
-    ):
+
+    def test_bad_request(self, bad_request: func.HttpRequest):
         actual_response = main(bad_request)
         assert actual_response.status_code == HTTPStatus.BAD_REQUEST
