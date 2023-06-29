c534dd0446fa79969752dec60e34436b5cf5c7bc
larisa <larisa.kosareva@hochfrequenz.de>
2023-06-29 15:07:38 +0200

    test GenerateAllFristen

---
+++ unittests/test_generate_all_fristen.py
@@ -0,0 +1,71 @@
+from http import HTTPStatus
+
+import azure.functions as func
+import pytest  # type:ignore[import]
+
+from GenerateAllFristen import main
+
+
+class TestGenerateAllFristen:
+    @pytest.mark.parametrize(
+        "ok_request",
+        [
+            pytest.param(
+                func.HttpRequest(
+                    "GET",
+                    "testhost/GenerateAllFristen",
+                    route_params={"year": "2023"},
+                    body=bytes(),
+                )
+            ),
+        ],
+    )
+    def test_ok_get(self, ok_request: func.HttpRequest):
+        actual_response = main(ok_request)
+        assert actual_response.status_code == HTTPStatus.OK
+        body = actual_response.get_body()
+        assert body is not None
+        
+
+    @pytest.mark.parametrize(
+        "bad_request",
+        [
+            pytest.param(
+                func.HttpRequest(
+                    "GET",
+                    "testhost/GenerateAllFristen",
+                    route_params={"year": "hhhh"},
+                    body=bytes(),
+                )
+            ),
+            pytest.param(
+                func.HttpRequest(
+                    "GET",
+                    "testhost/GenerateAllFristen",
+                    route_params={"year": "0"},
+                    body=bytes(),
+                )
+            ),
+            
+            pytest.param(
+                func.HttpRequest(
+                    "GET",
+                    "testhost/GenerateAllFristen",
+                    route_params={},
+                    body=bytes(),
+                )
+            ),
+            pytest.param(
+                func.HttpRequest(
+                    "GET",
+                    "testhost/GenerateFristenForType",
+                    body=bytes(),
+                )
+                
+            ),
+        ],
+    )
+
+    def test_bad_request(self, bad_request: func.HttpRequest):
+        actual_response = main(bad_request)
+        assert actual_response.status_code == HTTPStatus.BAD_REQUEST
