dc5bc58d55cb1cc69c3c630c2b4f899b2533ae19
larisa <larisa.kosareva@hochfrequenz.de>
2023-06-29 08:44:42 +0200

    add function GenereteAndExportWholeCalendar

---
+++ GenerateAndExportWholeCalendar/__init__.py
@@ -0,0 +1,67 @@
+import json
+import logging
+from http import HTTPStatus
+
+import azure.functions as func
+from fristenkalender_generator.bdew_calendar_generator import (
+    FristenkalenderGenerator,
+    FristenType,
+    FristWithAttributes,
+    FristWithAttributesAndType,
+)
+from pathlib import Path
+import tempfile
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Python HTTP trigger function processed a request.")
+    try:
+        filename = req.route_params.get("filename")
+        attendee = req.route_params.get("attendee")
+        year_params = req.route_params.get("year")
+        if not filename:
+            raise ValueError("filename should not be empty")
+        if not attendee:
+            raise ValueError("Attendee should not be empty")
+        if not year_params:
+            raise TypeError("Year should not be empty")
+        else:
+            year = int(year_params)
+    except TypeError as type_err:
+        logging.warning("Request parametr is invalid: %s", str(type_err))
+        return func.HttpResponse(
+            body=json.dumps({"error": str(type_err), "code": HTTPStatus.BAD_REQUEST}),
+            status_code=HTTPStatus.BAD_REQUEST,
+            mimetype="application/json",
+        )
+    except ValueError as val_error:
+        logging.warning("Request parametr is invalid: %s", str(val_error))
+        return func.HttpResponse(
+            body=json.dumps({"error": str(val_error), "code": HTTPStatus.BAD_REQUEST}),
+            status_code=HTTPStatus.BAD_REQUEST,
+            mimetype="application/json",
+        )
+    logging.info(
+        "Generating an ics-calendar with this parameters path='%s', attendee='%s' year='%s'",
+        filename,
+        attendee,
+        year,
+    )
+
+    try:
+        local_ics_file_path: Path
+        with tempfile.NamedTemporaryFile(mode="w+", suffix=".ics", delete=False) as tmp_file:
+            local_ics_file_path = Path(tmp_file.name)
+            FristenkalenderGenerator().generate_and_export_whole_calendar(local_ics_file_path, attendee, year)
+        with open(local_ics_file_path, "rb") as ics_file:
+            print("i am here")
+            file_body = ics_file.read()
+        return func.HttpResponse(
+            body= file_body,
+            status_code=HTTPStatus.OK,
+            mimetype="text/calendar",
+        )
+    except TypeError as type_error:
+        print(type_error)
+    except Exception as general_error:
+        print(general_error)
